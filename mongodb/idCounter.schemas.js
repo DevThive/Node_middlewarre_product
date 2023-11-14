4b70685cf17a1fbc960fe57b745df8cae7ed7fe6
데브티브 <thive8564@gmail.com>
2023-11-14 19:47:46 +0900

    add product delete ( middleware)

--- mongodb/idCounter.schemas.js
+++ mongodb/idCounter.schemas.js
@@ -7,7 +7,6 @@ const idCounterSchema = new mongoose.Schema({
   },
   count: {
     type: Number,
-    default: 0,
   },
 });
 module.exports = mongoose.model("idCounter", idCounterSchema);
--- routes/products.js
+++ routes/products.js
@@ -13,7 +13,7 @@ router.get("/products", async (req, res) => {
   res.send(products);
 });
 // 상품 상세 조회
-router.get("/products/:productId", (req, res) => {});
+router.get("/product/:productId", (req, res) => {});
 
 // 상품 생성 (인증 미들웨어 사용)
 router.post("/product", authMiddleware, async (req, res) => {
@@ -21,37 +21,62 @@ router.post("/product", authMiddleware, async (req, res) => {
   const { email } = res.locals.user;
 
   const product = Products.findOne({ Product_name });
-
+  const idCounter = await IdCounter.findOne({ model: "product2" });
   if (product.length) {
     return res.status(400).send({ errorMessage: "상품이 이미 존재합니다." });
   }
 
-  const idCounter = await IdCounter.findOne({ model: "products" });
   if (!idCounter) {
     // 처음 등록할 경우 counter 생성
-    await IdCounter.create({ model: "products", count: 1 });
+    await IdCounter.create({ model: "product2", count: 1 });
   } else {
     // 이미 있다면 counter 증가
     idCounter.count++;
     await idCounter.save();
   }
   const productsId = idCounter.count;
+  try {
+    const createProduct = await Products.create({
+      Product_id: productsId,
+      Product_name: Product_name,
+      Product_desc: Product_desc,
+      User_name: email,
+    });
 
-  const createProduct = await Products.create({
-    Product_id: productsId,
-    Product_name: Product_name,
-    Product_desc: Product_desc,
-    User_name: email,
-  });
-
-  res.json({ products: createProduct, Message: "save Success" });
+    res.status(200).send({ products: createProduct, Message: "save Success" });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.delete("/product/:productId", async (req, res) => {
+router.delete("/product/:productId", authMiddleware, async (req, res) => {
   const { productId } = req.params;
+  const { email } = res.locals.user;
 
   const product = await Products.findOne({ Product_id: productId });
+
+  if (!product) {
+    res.status(401).send({ errorMessage: "상품이 존재하는지 확인해주세요" });
+    return false;
+  }
+
+  if (product.User_name !== email) {
+    res
+      .status(401)
+      .send({ errorMessage: "본인이 작성한 글만 삭제가 가능합니다." });
+  }
+
+  try {
+    await Products.deleteOne({ Product_id: productId });
+    res.status(200).send({ Message: "삭제 되었습니다." });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 //상품 수정 (인증 미들웨어 사용)
+router.put("/product/:productId", authMiddleware, async (req, res) => {
+  const { productId } = req.params;
+  const { Product_name, Product_desc, State } = req.body;
+});
 module.exports = router;
